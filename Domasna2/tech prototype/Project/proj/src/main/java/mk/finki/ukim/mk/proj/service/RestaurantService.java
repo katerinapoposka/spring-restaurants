419daf1261b9d247fbcfe9281d51471786a8c44c
AngelNasev <angel.nasev@gmail.com>
2022-12-20 13:04:40 +0100

    Added paging to all restaurants

--- Domasna2/tech prototype/Project/proj/src/main/java/mk/finki/ukim/mk/proj/service/RestaurantService.java
+++ Domasna2/tech prototype/Project/proj/src/main/java/mk/finki/ukim/mk/proj/service/RestaurantService.java
@@ -2,12 +2,16 @@ package mk.finki.ukim.mk.proj.service;
 
 import mk.finki.ukim.mk.proj.model.CuisineType;
 import mk.finki.ukim.mk.proj.model.Restaurants;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 
 import java.util.List;
 import java.util.Optional;
 
 public interface RestaurantService {
     List<Restaurants> getAllRestaurants();
+    Page<Restaurants> getAllRestaurantsPageable(Pageable pageable);
     Optional<Restaurants> getRestaurantById(Long id);
     List<Restaurants> getRestaurantByName(String name);
     List<Restaurants> getRestaurantByCuisine(String cuisine);
