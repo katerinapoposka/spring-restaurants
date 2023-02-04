ef8ff01a0e5eb5c1356c2e824f7525e472704646
AngelNasev <angel.nasev@gmail.com>
2023-02-04 15:45:54 +0100

    Directory name change

---
+++ Domasna3 i 4/Project/proj/src/main/java/mk/finki/ukim/mk/proj/model/Role.java
@@ -0,0 +1,12 @@
+package mk.finki.ukim.mk.proj.model;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
+    ROLE_USER;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
+}
